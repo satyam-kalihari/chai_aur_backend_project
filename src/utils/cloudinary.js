4a103b6277511321b37ff950efe83f66017a469e
satyam-kalihari <satyamkalihari17@gmail.com>
2024-08-17 15:49:25 +0530

    cloudinary.js

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs"
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
+
+const updateOnCloudinary = async (localFilePath) => {
+    try{
+        if(!localFilePath) return null
+        //upload the file on cloudinary
+        const responce = await cloudinary.uploader.upload(localFilePath, {resource_type:"auto"})
+        console.log("File uploaded on cloudinary", responce.url)
+        return responce.url
+    }catch{
+        console.log("Error updating image on Cloudinary")
+        fs.unlinkSync(localFilePath)
+
+        return null
+    }
+}
+
+export {updateOnCloudinary}
